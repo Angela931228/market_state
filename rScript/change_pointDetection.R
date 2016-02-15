118779aab6f1a0e244caa2edc533e691b4727e45
Angela <zaq931228@gmail.com>
2016-02-15 10:47:54 +0800

    fixed split and merge issue
    change segmentation by dates to by  index to avoid the holiday problem

--- rScript/change_pointDetection.R
+++ rScript/change_pointDetection.R
@@ -4,10 +4,8 @@ library("ggplot2")
 library("EMD")
 library("xlsx")
 library("e1071")
-library("TSdist")
-library("TSclust")
 
-setwd("C:/Users/angela.zhou/Desktop/angela_dissertation/market_state")
+setwd("/Users/Angela/Desktop/market_state")
 hsi_price<- read.xlsx("./data/hk/hsi_index_10year.xlsx",1)
 hsi_emd <- emd(as.numeric(as.vector(hsi_price[,2])),hsi_price[,1],boundary="wave")
 hsi_emd_res <- as.numeric(as.vector(hsi_price[,2]))- hsi_emd$imf[,1] - hsi_emd$imf[,2] - hsi_emd$imf[,3] 
@@ -21,12 +19,20 @@ axis(1, at=label_inteval, labels=label_date,las=2)
 firstD<- diff(hsi_emd_res) 
 
 result<- {}
-result<- rbind(result,hsi_price[1,1])
-
-for(i in 1:(length(firstD)-1)){
+result<- rbind(result,1)
+#for(i in 1:(length(firstD)-1)){
     if(firstD[i]*firstD[i+1]<0){
         result<- rbind(result, hsi_price[i+1,1])
     }
+#}
+for(i in 1:(length(firstD)-1)){
+  if(firstD[i]*firstD[i+1]<0){
+    result<- rbind(result,i+1)
+  }
+}
+# handle the last point
+if(result[length(result)]!=nrow(hsi_price)){
+  result<- rbind(result, nrow(hsi_price))
 }
 segDuration<- diff(result)
 
@@ -34,10 +40,10 @@ segDuration<- diff(result)
 splitSequence <- function(segDuration){
       #split first
       for(i in 1: length(segDuration)){
-        if(segDuration[i]>60){
+        if(segDuration[i]>40){
           half<-segDuration[i]/2
           segDuration[i]<-ceiling(half)
-          segDuration <- insertInto(segDuration,i+1,floor(half))
+          segDuration <- insertInto(segDuration,i,floor(half))
         }
       }
     return(segDuration)
@@ -45,9 +51,8 @@ splitSequence <- function(segDuration){
 mergeSequence <- function(segDuration){
   #split first
   for(i in 1: (length(segDuration))){
-    if(length(segDuration)>i){
-      if(segDuration[i]<20){
-         
+    if(length(segDuration)>=i){
+      if(segDuration[i]<15){
            if(i==1){
               segDuration[i+1]<- segDuration[i]+segDuration[i+1]
               segDuration<-deleteBy(segDuration,i)
@@ -67,13 +72,11 @@ mergeSequence <- function(segDuration){
   return(segDuration)
 }
 insertInto <- function(DF, position,data){
-    if((position-1)>=1){
-      d_result <- append(DF[c(1:(position-1))],data)
-    }
-    if((position+1)<=length(DF)){
-      d_result<-append(d_result,DF[c((position+1):length(DF))])
-    }
-    return(d_result)
+ 
+      d_result <- append(DF[c(1:(position))],data)
+      if(position+1<=length(DF))
+        d_result<-append(d_result,DF[c((position+1):length(DF))])
+      return(d_result)
 }
 deleteBy <- function(DF, position){
     if((position-1)>=1&&(position+1)<=length(DF)){
@@ -88,9 +91,9 @@ deleteBy <- function(DF, position){
 rTest<-splitSequence(segDuration)
 rTest<-splitSequence(rTest)
 rMergeTest<-mergeSequence(rTest)
-rMergeTest<-splitSequence(rMergeTest) 
+rMergeTest<-splitSequence(rMergeTest)
+rMergeTest<-splitSequence(rMergeTest)
 
-which(hsi_price[,1]==(hsi_price[1,1]+43))
 
 cl1_price<- read.xlsx("./data/hk/cl1_10year.xlsx",1)
 cl1_emd <- emd(as.numeric(as.vector(cl1_price[,2])),cl1_price[,1],boundary="wave")
@@ -127,19 +130,47 @@ getPastOneMonthTrend <- function(data, date){
   
   
 }
-currentIndex<-1
-for(i in 1:length(rMergeTest)){
-    print(i)
-    print(currentIndex)
-    print(hsi_price[currentIndex,1]+(rMergeTest[i]))
+
+#for(i in 1:length(rMergeTest)){
     if(length(which(hsi_price[,1]==(hsi_price[currentIndex,1]+(rMergeTest[i]))))!=0){
       newIndex<-which(hsi_price[,1]==(hsi_price[currentIndex,1]+(rMergeTest[i])))
     }else if(length(which(hsi_price[,1]==(hsi_price[currentIndex,1]+(rMergeTest[i]+1))))!=0) {
       newIndex<-which(hsi_price[,1]==(hsi_price[currentIndex,1]+(rMergeTest[i]+1)))
     }else if(length(which(hsi_price[,1]==(hsi_price[currentIndex,1]+(rMergeTest[i]+2))))!=0){
       newIndex<-which(hsi_price[,1]==(hsi_price[currentIndex,1]+(rMergeTest[i]+2)))
-    }else {
+    }else if(length(which(hsi_price[,1]==(hsi_price[currentIndex,1]+(rMergeTest[i]+3))))!=0){
+      newIndex<-which(hsi_price[,1]==(hsi_price[currentIndex,1]+(rMergeTest[i]+3)))
+    }else if(length(which(hsi_price[,1]==(hsi_price[currentIndex,1]+(rMergeTest[i]+4))))!=0){
+      newIndex<-which(hsi_price[,1]==(hsi_price[currentIndex,1]+(rMergeTest[i]+4)))
+    }else if(length(which(hsi_price[,1]==(hsi_price[currentIndex,1]+(rMergeTest[i]+5))))!=0){
       newIndex<-which(hsi_price[,1]==(hsi_price[currentIndex,1]+(rMergeTest[i]+5)))
     }
+    print(currentIndex)
+    print(newIndex)
+    print(rMergeTest[i])
+    print(hsi_price[newIndex,2])
+    print((hsi_emd_res[newIndex]-hsi_emd_res[currentIndex])/hsi_emd_res[currentIndex])
     currentIndex<- newIndex
+#}
+currentIndex<-1
+changeRates<-{}
+comIndex <-{}
+comIndex <- rbind(comIndex,currentIndex )
+signals <- {}
+for(i in 1:length(rMergeTest)){
+  newIndex<- currentIndex + rMergeTest[i]
+  changeRate <- round((hsi_emd_res[newIndex]-hsi_emd_res[currentIndex])/hsi_emd_res[currentIndex],digits=2) 
+  if(changeRate>=0.02){
+    signals<- append(signals, 'U')
+  }else if(changeRate <= -0.02){
+    signals<- append(signals,'D')
+  }else {
+    signals<- append(signals,'S')
+  }
+  changeRates <- rbind(changeRates,changeRate)
+  currentIndex<- newIndex
+  comIndex <- rbind(comIndex,currentIndex )
 }
+write.csv(hsi_price[comIndex,1],"segIndex.csv")
+
+write.csv(changeRates, "changeRates.csv")
